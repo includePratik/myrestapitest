38463318f66c30cc219bb4bc7a402128d683a3e3
includePratik <31407647+includePratik@users.noreply.github.com>
2017-10-12 20:52:10 +0530

    Fist Commit

--- temp.js
+++ temp.js
@@ -70,21 +70,22 @@ console.log('Program Started');
 //
 // }).listen(8004);
 
-
-var net = require('net');
-
-var server = net.createServer(function(socket) {
-    socket.write('Echo server\r\n');
-   // socket.pipe(socket);
-});
-
-server.listen(process.env.PORT || 3000, function() {
-    console.log("listening on 3000");
-});
 //
-// var http = require('http')
-// http.createServer(function (req,res) {
-//    res.writeHead(200,{'Content-type':'text/plain'})
-//    res.write("Echo server ")
-//    res.end()
-// }).listen(1337);
+// var net = require('net');
+//
+// var server = net.createServer(function(socket) {
+//     socket.write('Echo server\r\n');
+//    // socket.pipe(socket);
+// });
+//
+// server.listen(process.env.PORT || 3000, function() {
+//     console.log("listening on 3000");
+// });
+
+//
+var http = require('http')
+http.createServer(function (req,res) {
+   res.writeHead(200,{'Content-type':'text/plain'})
+   res.write("Echo server ")
+   res.end()
+}).listen(3000);
